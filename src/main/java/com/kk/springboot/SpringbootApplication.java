9cf9a18d08485cc2f286c96766133b9874039ecc
Krishan Kumar <kktech970@gmail.com>
2021-05-07 09:23:22 +0530

    Configured H2

--- src/main/java/com/kk/springboot/SpringbootApplication.java
+++ src/main/java/com/kk/springboot/SpringbootApplication.java
@@ -1,13 +1,31 @@
 package com.kk.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kk.springboot.entity.Course;
+import com.kk.springboot.repository.CourseRepository;
+
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements CommandLineRunner {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private CourseRepository courseRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Course course = courseRepo.findById(1);
+		logger.info("Course id=1 >> " + course);
 	}
 
 }
