da68be94100bd16b934309e8d0321658c57c76f0
Krishan Kumar <kktech970@gmail.com>
2021-05-08 18:10:40 +0530

    Delete method is added in test.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 insert into course(id, name) values(1, 'Spring boot course');
+insert into course(id, name) values(2, 'Spring JPA course');
+insert into course(id, name) values(3, 'Spring Webflux course');
--- src/test/java/com/kk/springboot/repository/CourseRepositoryTest.java
+++ src/test/java/com/kk/springboot/repository/CourseRepositoryTest.java
@@ -1,27 +1,40 @@
 package com.kk.springboot.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import com.kk.springboot.entity.Course;
 
 @SpringBootTest
 public class CourseRepositoryTest {
 	
-	Logger logger = LoggerFactory.getLogger(CourseRepositoryTest.class);
+	private Logger logger = LoggerFactory.getLogger(CourseRepositoryTest.class);
 	
 	@Autowired
-	CourseRepository courseRepo;
+	private CourseRepository courseRepo;
 	
 	@Test
 	void testFindById() {
-		Course course = courseRepo.findById(1);
-		assertEquals("Spring boot course", course.getName());
+		Course course = courseRepo.findById(3);
+		assertEquals("Spring Webflux course", course.getName());
+	}
+	
+	@Test
+	@DirtiesContext
+	/**
+	 * Problem : Following test will actually delete the value from DB which can be used by another test or application. 
+	 * @DirtiesContext tells spring to recover the updated/deleted value after executing the test.
+	 */
+	void testDeleteById() {
+		courseRepo.deleteById(2);
+		assertNull(courseRepo.findById(2));
 	}
 	
 }
