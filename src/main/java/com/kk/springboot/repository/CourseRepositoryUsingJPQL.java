a7d00ac1afc6567ca388bed6b37eeff466ae912f
Krishan Kumar <kktech970@gmail.com>
2021-05-23 17:15:12 +0530

    Removing unused variable.

--- src/main/java/com/kk/springboot/repository/CourseRepositoryUsingJPQL.java
+++ src/main/java/com/kk/springboot/repository/CourseRepositoryUsingJPQL.java
@@ -7,8 +7,6 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
