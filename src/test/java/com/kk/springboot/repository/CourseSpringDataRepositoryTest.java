37c9766ffca722d833e6e924f60f479a440ba997
Krishan Kumar <kktech970@gmail.com>
2021-05-29 09:55:08 +0530

    Spring data jpa sorting.

--- src/test/java/com/kk/springboot/repository/CourseSpringDataRepositoryTest.java
+++ src/test/java/com/kk/springboot/repository/CourseSpringDataRepositoryTest.java
@@ -3,11 +3,14 @@ package com.kk.springboot.repository;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 import com.kk.springboot.entity.Course;
 
@@ -29,6 +32,15 @@ public class CourseSpringDataRepositoryTest {
 		Optional<Course> courseOptional = courseSpringDataRepo.findById(99L);
 		System.out.println("testCourseNotPresent() >> " + courseOptional.isPresent());
 		assertFalse(courseOptional.isPresent());
+	}
+	
+	@Test
+	public void testCourseSort() {
+		Sort sort = Sort.by(Direction.DESC, "name");//.and(Sort); --Like this you can add more operations.
+		List<Course> allCourseList = courseSpringDataRepo.findAll(sort);
+		System.out.println("testCourseSort() >> ");
+		allCourseList.forEach(course -> System.out.println(course));
+		assertFalse(allCourseList.isEmpty());
 	}
 	
 }
