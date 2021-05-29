62872b932b65f5a6f70036f16690acd6750033d4
Krishan Kumar <kktech970@gmail.com>
2021-05-29 14:08:08 +0530

    Spring data jpa custom queries.

--- src/main/java/com/kk/springboot/repository/CourseSpringDataRepository.java
+++ src/main/java/com/kk/springboot/repository/CourseSpringDataRepository.java
@@ -1,11 +1,28 @@
 package com.kk.springboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kk.springboot.entity.Course;
 
 @Repository
-public interface CourseSpringDataRepository extends JpaRepository<Course, Long>{
+public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
+	List<Course> findByName(String name);
 	
+	List<Course> deleteByName(String name);
+	
+	List<Course> findByNameOrderByIdDesc(String name);
+	
+	List<Course> countByName(String name);
+	
+	List<Course> findByNameAndId(String name, Long id);
+	
+	@Query("select c from Course c where c.id = :id")
+	List<Course> customJPQL(Long id);
+	
+	@Query(value = "select * from Course where id = :id" ,nativeQuery = true)
+	List<Course> customNativeQuery(Long id);
 }
